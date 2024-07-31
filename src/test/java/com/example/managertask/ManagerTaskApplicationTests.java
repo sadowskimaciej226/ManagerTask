e1ac917bdc9eb46684886a5f987bcd52e96b5277
sadow <sadowskimaciej226@gmail.com>
2024-07-31 12:44:54 +0200

    added tests to project

---
+++ src/test/java/com/example/managertask/ManagerTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
