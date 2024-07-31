a218e71f41bc077322b8c33cdc07738c748696de
sadow <sadowskimaciej226@gmail.com>
2024-07-31 12:44:01 +0200

    prepared appliaction to start

---
+++ src/main/java/com/example/managertask/ManagerTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.managertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerTaskApplication.class, args);
+    }
+
+}
