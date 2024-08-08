78cd6c31d4efb0da25f217710c5cdd87d82fd37b
sadow <sadowskimaciej226@gmail.com>
2024-08-08 11:36:43 +0200

    added method to find by email client to repository and service

--- src/main/java/com/example/managertask/client/ClientRepository.java
+++ src/main/java/com/example/managertask/client/ClientRepository.java
@@ -2,5 +2,8 @@ package com.example.managertask.client;
 
 import org.springframework.data.repository.CrudRepository;
 
- interface ClientRepository extends CrudRepository<Client, Long> {
+import java.util.Optional;
+
+interface ClientRepository extends CrudRepository<Client, Long> {
+  Optional<Client> findClientByEmail(String email);
 }
--- src/main/java/com/example/managertask/client/ClientService.java
+++ src/main/java/com/example/managertask/client/ClientService.java
@@ -1,21 +1,21 @@
 package com.example.managertask.client;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class ClientService {
     private final ClientRepository clientRepository;
     private final ClientDtoMapper clientDtoMapper;
 
-
-    public ClientService(ClientRepository clientRepository, ClientDtoMapper clientDtoMapper) {
-        this.clientRepository = clientRepository;
-        this.clientDtoMapper = clientDtoMapper;
-    }
     public Optional<ClientDto> getUserById(Long id) {
         return clientRepository.findById(id)
                 .map(clientDtoMapper::map);
+    }
+    public Optional<Client> getUserByEmail(String email){
+       return clientRepository.findClientByEmail(email);
     }
 }
