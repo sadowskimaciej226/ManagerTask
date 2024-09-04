c2bde9ed1a8f77f12873f03c56d14aef1dc7caa6
sadow <sadowskimaciej226@gmail.com>
2024-09-04 15:50:20 +0200

    implemented new method in repo to get accurate task

--- src/main/java/com/example/managertask/task/TaskRepository.java
+++ src/main/java/com/example/managertask/task/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.example.managertask.task;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDateTime;
@@ -8,4 +9,9 @@ import java.util.List;
 interface TaskRepository extends CrudRepository<Task, Long> {
     List<Task> findByExpirationTimeAfterAndClientId(LocalDateTime now,Long id);
     List<Task> findByPeriodicityAndExpirationTimeAfter(Periodicity periodicity, LocalDateTime now);
+    @Query("SELECT t FROM Task t where t.client.email = ?1 AND ((t.startTime > ?2 OR t.expirationTime < ?3) " +
+            "OR t.periodicity = com.example.managertask.task.Periodicity.EVERY_DAY)")
+    List<Task> findCurrentTasks(String email, LocalDateTime start, LocalDateTime expiration);
+    List<Task> findAllByPeriodicity(Periodicity periodicity);
+
  }
