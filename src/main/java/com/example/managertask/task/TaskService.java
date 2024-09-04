f4eb76ddb37072c37e6e4277d80f3a19c6dd0997
sadow <sadowskimaciej226@gmail.com>
2024-09-04 16:05:20 +0200

    implemented methods for get task for today

--- src/main/java/com/example/managertask/task/TaskService.java
+++ src/main/java/com/example/managertask/task/TaskService.java
@@ -3,11 +3,13 @@ package com.example.managertask.task;
 
 import com.example.managertask.client.Client;
 import com.example.managertask.client.ClientRepository;
+import com.example.managertask.client.ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +21,7 @@ public class TaskService {
     private final TaskRepository taskRepository;
     private final TaskDtoMapper taskDtoMapper;
     private final ClientRepository clientRepository;
+    private final ClientService clientService;
 
 
     Optional<TaskDto> getTaskById(Long id){
@@ -28,6 +31,9 @@ public class TaskService {
 
     TaskDto saveTask(TaskDto taskDto) {
         Task taskToSave = taskDtoMapper.map(taskDto);
+        if(taskToSave.getStartTime() == null) {
+            taskToSave.setStartTime(LocalDateTime.now());
+        }
         Task savedTask = taskRepository.save(taskToSave);
         return taskDtoMapper.map(savedTask);
     }
@@ -59,9 +65,17 @@ public class TaskService {
         List<Task> taskToChange = taskRepository
                 .findByPeriodicityAndExpirationTimeAfter(periodicity, now);
         taskToChange.forEach(task -> {
-            task.setExpirationTime(now.plusHours(hours));
+            task.setExpirationTime(task.getExpirationTime().plusHours(hours));
             taskRepository.save(task);
         });
+    }
+    List<TaskDto> getAllCurrentUserTaskForToday(){
+        LocalDateTime midnight = LocalDateTime.now().with(LocalTime.MIDNIGHT);
+        String currentEmail = clientService.getCurrentUsername();
+         return taskRepository.findCurrentTasks(currentEmail, midnight, midnight.plusHours(24))
+                 .stream()
+                 .map(taskDtoMapper::map)
+                 .toList();
     }
 
 
@@ -73,6 +87,7 @@ public class TaskService {
     @Scheduled(cron = "0 0 0 * * SUN")
     void updateExpirationTimeEverySunday(){
         setNewExpirationTime(Periodicity.EVERY_WEEK, 24*7);
+
     }
     @Scheduled(cron = "0 0 0 1 * ?")
     void updateExpirationTimeEveryMonth(){
