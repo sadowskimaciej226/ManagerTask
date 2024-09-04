7dbe4f68ddb4747f33f9325eb5cd0057f3b79b4c
sadow <sadowskimaciej226@gmail.com>
2024-09-04 16:05:40 +0200

    added basic tests for client Controller

--- src/test/java/com/example/managertask/client/ClientServiceTest.java
+++ src/test/java/com/example/managertask/client/ClientServiceTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -18,30 +19,41 @@ import java.util.Optional;
 
 
 public class ClientServiceTest {
+    @Mock
+    private SecurityContext securityContext;
+
+    @Mock
+    private Authentication authentication;
 
     @Mock
     private ClientRepository clientRepository;
     @Mock
     private ClientDtoMapper clientDtoMapper;
 
+    @Spy
     @InjectMocks
     ClientService clientService;
 
     ClientDto dto;
+    Client client;
     @BeforeEach
     void setUp(){
         MockitoAnnotations.openMocks(this);
+        client = new Client();
+        dto = new ClientDto();
+
+        SecurityContextHolder.setContext(securityContext);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
     }
 
     @Test
     public void clientServiceShouldReturnClientDto(){
 
         //given
-        Client client = new Client();
+
         client.setId(1L);
         client.setFirstName("Someone");
 
-        ClientDto dto = new ClientDto();
         dto.setId(client.getId());
         client.setFirstName(client.getFirstName());
 
@@ -65,6 +77,50 @@ public class ClientServiceTest {
         Mockito.when(clientRepository.findById(1L)).thenReturn(Optional.empty());
         //then
         assertThrows(NoSuchElementException.class, () -> clientService.getUserById(1L).orElseThrow());
+    }
+    @Test
+    public void shouldReturnUserDtoOnlyIfItMatchesJwtSubject(){
+
+        //given
+        Client client = new Client();
+        client.setId(1L);
+        client.setEmail("someone@mail.com");
+        dto.setId(1L);
+
+
+        when(clientService.getCurrentUsername()).thenReturn("someone@mail.com");
+
+        Mockito.when(clientRepository.findById(1L)).thenReturn(Optional.of(client));
+        Mockito.when(clientDtoMapper.map(client)).thenReturn(dto);
+
+        //when
+        Optional<ClientDto> userForCurrentUser = clientService.getUserForCurrentUser(1L);
+
+        //then
+
+      assertThat(userForCurrentUser.get().getId(), equalTo(1L));
+      assertThat(userForCurrentUser, not(Optional.empty()));
+
+    }
+
+    @Test
+    public void shouldReturnUserDtoOnlyIfJwtNotMatches(){
+
+        //given
+        client.setId(1L);
+        client.setEmail("lol@mail.com");
+        dto.setId(1L);
+
+
+        when(clientService.getCurrentUsername()).thenReturn("someone@mail.com");
+
+        Mockito.when(clientRepository.findById(1L)).thenReturn(Optional.of(client));
+        Mockito.when(clientDtoMapper.map(client)).thenReturn(dto);
+
+        //when
+        //then
+        assertThrows(NoSuchElementException.class, () -> clientService.getUserForCurrentUser(1L).orElseThrow());
+
     }
 
 
