ce4573e1121d5f53c55800eb5e5fcfeb0e5d849b
sadow <sadowskimaciej226@gmail.com>
2024-09-06 15:01:27 +0200

    implemented all tests for ClientService

--- src/test/java/com/example/managertask/client/ClientServiceTest.java
+++ src/test/java/com/example/managertask/client/ClientServiceTest.java
@@ -1,10 +1,15 @@
 package com.example.managertask.client;
 
+import com.example.managertask.client.invite.Invitation;
+import com.example.managertask.client.invite.InvitationRepository;
+import com.example.managertask.family.Family;
+import com.example.managertask.family.FamilyRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
 import org.mockito.*;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,9 +26,12 @@ import java.util.Optional;
 public class ClientServiceTest {
     @Mock
     private SecurityContext securityContext;
-
+    @Mock
+    private FamilyRepository familyRepository;
     @Mock
     private Authentication authentication;
+    @Mock
+    private InvitationRepository invitationRepository;
 
     @Mock
     private ClientRepository clientRepository;
@@ -104,7 +112,7 @@ public class ClientServiceTest {
     }
 
     @Test
-    public void shouldReturnUserDtoOnlyIfJwtNotMatches(){
+    public void shouldThrowExceptionIfJwtNotMatches(){
 
         //given
         client.setId(1L);
@@ -120,6 +128,106 @@ public class ClientServiceTest {
         //when
         //then
         assertThrows(NoSuchElementException.class, () -> clientService.getUserForCurrentUser(1L).orElseThrow());
+
+    }
+    @Test
+    public void shouldThrowExceptionWhenUpdatingWrongUser(){
+
+        //given
+        client.setEmail("someone@mail.com");
+
+        when(clientService.getCurrentUsername()).thenReturn("someoneElse@mail.com");
+        Mockito.when(clientDtoMapper.map(dto)).thenReturn(client);
+
+        //when
+        //then
+        assertThrows(IllegalArgumentException.class, () -> clientService.updateUser(dto));
+    }
+
+    @Test
+    public void shouldCallSaveMethodWhenSubjectMatches(){
+        //given
+        client.setEmail("someone@mail.com");
+
+        when(clientService.getCurrentUsername()).thenReturn("someone@mail.com");
+        Mockito.when(clientDtoMapper.map(dto)).thenReturn(client);
+
+        //when
+        clientService.updateUser(dto);
+        //then
+        verify(clientRepository, times(1)).save(client);
+
+    }
+
+    @Test
+    public void doNotCallSaveInvitationWhenItAlreadyExists(){
+
+        //given
+
+        Client invitee = new Client();
+        invitee.setFirstName("Ty");
+        ClientDto inviteeDto = new ClientDto();
+        Family family = new Family();
+        client.setFamily(family);
+        family.setId(1L);
+        Invitation invitation = new Invitation();
+
+        when(clientDtoMapper.map(dto)).thenReturn(client);
+        when(clientDtoMapper.map(inviteeDto)).thenReturn(invitee);
+        when(familyRepository.findById(1L)).thenReturn(Optional.of(family));
+        when(invitationRepository.findByFamilyAndInvitee(family,invitee)).thenReturn(Optional.of(invitation));
+
+        //when
+        clientService.inviteClientToFamily(dto, inviteeDto);
+
+        //then
+        verify(invitationRepository, times(0)).save(any());
+
+    }
+    @Test
+    public void doCallSaveInvitationWhenItNotExists(){
+
+        //given
+
+        Client invitee = new Client();
+        invitee.setFirstName("Ty");
+        ClientDto inviteeDto = new ClientDto();
+        Family family = new Family();
+        client.setFamily(family);
+        family.setId(1L);
+
+
+        when(clientDtoMapper.map(dto)).thenReturn(client);
+        when(clientDtoMapper.map(inviteeDto)).thenReturn(invitee);
+        when(familyRepository.findById(1L)).thenReturn(Optional.of(family));
+        when(invitationRepository.findByFamilyAndInvitee(family,invitee)).thenReturn(Optional.empty());
+
+        //when
+        clientService.inviteClientToFamily(dto, inviteeDto);
+
+        //then
+        verify(invitationRepository, times(1)).save(any());
+
+    }
+    @Test
+    public void shouldDeleteInvitationAfterJoining(){
+
+        //given
+        Family family = new Family();
+        Invitation invitation = new Invitation();
+        invitation.setFamily(family);
+
+        when(familyRepository.findById(1L)).thenReturn(Optional.of(family));
+        when(clientRepository.findById(1L)).thenReturn(Optional.of(client));
+        when(invitationRepository.findByFamilyAndInvitee(family, client)).thenReturn(Optional.of(invitation));
+
+        //when
+        clientService.joinFamily(1L, 1L);
+
+        //then
+        assertThat(client.getFamily(), equalTo(family));
+        verify(clientRepository, times(1)).save(any());
+        verify(invitationRepository, times(1)).delete(any());
 
     }
 
